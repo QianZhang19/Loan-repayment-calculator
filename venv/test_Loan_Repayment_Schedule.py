f5b3bb3f2c4fd46e24f5be67b1356442b6161dfe
QianZhang19 <qianzhang019@gmail.com>
2023-01-29 17:48:51 +0000

    test

--- venv/test_Loan_Repayment_Schedule.py
+++ venv/test_Loan_Repayment_Schedule.py
@@ -1,2 +1,13 @@
 from Loan_Repayment_Schedule import delButton
 from Loan_Repayment_Schedule import calc
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+tree = ttk.Treeview(root, show="headings")
+root = None
+
+
+def test_delButton():
+    output = delButton(tree)
+    assert output == root
